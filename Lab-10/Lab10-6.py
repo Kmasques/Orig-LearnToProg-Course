5cd0a00143f8ce287a43276c0640aef1caa21d24
Kathedral Bayl <kathedral.bayl@gmail.com>
2017-07-16 19:15:35 -0400

    Add Pythag file to Lab-10

--- Lab-10/Lab10-6.py
+++ Lab-10/Lab10-6.py
@@ -5,3 +5,14 @@ gpa = 3.66
 print "I am {} years old." .format(age)
 print "My name is {}." .format (name)
 print "In school my GPA was {}." .format(gpa)
+
+operand1 = 978.425
+operand2 = 0.0324
+
+print "{} + {} = {}" .format(operand1,operand2,operand1+operand2)
+print "{} - {} = {}" .format(operand1,operand2,operand1-operand2)
+print "{} * {} = {}" .format(operand1,operand2,operand1*operand2)
+print "{} / {} = {}" .format(operand1,operand2,operand1/operand2)
+
+print "{} % {} = {}" .format (9,3,9 % 3)
+print "{} % {} = {}" .format (112,10,112 % 10)
---
+++ Lab-10/pythag.py
@@ -0,0 +1,11 @@
+print "Pythagorean Theorem"
+
+import math
+
+sideA = input ("Length of side A is ")
+sideB = input ("Length of side B is ")
+sideC = (sideA * sideA) + (sideB * sideB)
+sideC = math.sqrt(sideC)
+print "Length of side C is {}" .format (sideC)
+
+
