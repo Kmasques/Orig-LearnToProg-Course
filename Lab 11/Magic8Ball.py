5346ddbf1f988593dc6456546375c94d9049967e
Kathedral Bayl <kathedral.bayl@gmail.com>
2017-07-16 22:14:47 -0400

    Add Lab 11

---
+++ Lab 11/Magic8Ball.py
@@ -0,0 +1,48 @@
+print "Magic 8 Ball"
+
+import random
+
+question = raw_input ("What is your question? ")
+answer = random.randint(1.20)
+if answer == 1
+    print "Positively Yes"
+elif answer == 2
+    print "Positively No"
+elif answer == 3
+    print "Positively Maybe"
+elif answer == 4
+    print "No need to know now"
+elif answer == 5
+    print "Busy. Ask again tomorrow"
+elif answer == 6
+    print "Repeat the Question"
+elif answer == 7
+    print "The Gods are with you"
+elif answer == 8
+    print "Angels are nodding in agreement"
+elif answer == 9
+    print "The Donkeys are braying. Not a good sign."
+elif answer == 10
+    print "Positively No"
+elif answer == 11
+    print "Positively Maybe"
+elif answer == 12
+    print "No need to know now"
+elif answer == 13
+    print "Busy. Ask again tomorrow"
+elif answer == 14
+    print "Repeat the Question"
+elif answer == 15
+    print "The Gods are with you"
+elif answer == 16
+    print "Angels are nodding in agreement"
+elif answer == 17
+    print "The Donkeys are braying. Not a good sign."
+elif answer == 18
+    print "Angels are nodding in agreement"
+elif answer == 19
+    print "The Donkeys are braying. Not a good sign."
+else:
+    print "Angels are nodding in agreement"
+
+
