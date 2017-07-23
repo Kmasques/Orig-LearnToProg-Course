bd47d27ebce338fa0f4dc604aa1537005d66726d
Kathedral Bayl <kathedral.bayl@gmail.com>
2017-07-22 21:28:52 -0400

    Add Lab-13

---
+++ Lab-13/Conversions.py
@@ -0,0 +1,9 @@
+def celciusToFarenheit(celciusTemp):
+    farenheit = celsiusTemperature * (9.0/5.0) + 32.0
+    return farenheit
+
+def farenheitToCelcius (farenheitTemmp):
+    celsius = (farenheitTemperature - 32.0) * (5.0/9.0)
+    return celcius
+
+
