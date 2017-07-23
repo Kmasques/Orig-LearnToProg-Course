1b68bbaa0e642fd98ac1c13a6d88370a23e9c9ee
Kathedral Bayl <kathedral.bayl@gmail.com>
2017-07-22 21:14:26 -0400

    Completed Lab-12

--- Lab-12/CompoundInterest.py
+++ Lab-12/CompoundInterest.py
@@ -3,16 +3,10 @@ numberOfYears = input("How many years would you like to invest your money?")
 annualInterestRate = input("What interest rate would you like to invest at? Enter as a decimal: (For 2.5% enter 0.025)")
 
 
-x=1
-print "Month\t,   Interest Earned\t,   Total To Date"
-
-while x <= (numberOfYears + 1):
+x = 1
+print "Month\tInterest Earned\tTotal To Date"
+while x < (numberOfYears*12) + 1:
     interestEarned = initialInvestment * (annualInterestRate/12)
     initialInvestment = interestEarned + initialInvestment
-    print x, "\t", "$", "{:.2f}".format(interestEarned), "\t", "$", "{:.2f}".format(initialInvestment)
-    if x == x + 1:
-        break
-
-
-
-
+    print x, "\t","$", "{:.2f}".format(interestEarned), "\t","$", "{:.2f}".format(initialInvestment)
+    x = x + 1
