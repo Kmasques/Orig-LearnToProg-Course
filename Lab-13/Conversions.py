588e994c5b6b1c7738b506fc8b7c1c806a652b5c
Kathedral Bayl <kathedral.bayl@gmail.com>
2017-07-22 21:42:12 -0400

    Add option A

--- Lab-13/Conversions.py
+++ Lab-13/Conversions.py
@@ -1,9 +1,19 @@
-def celciusToFarenheit(celciusTemp):
-    farenheit = celsiusTemperature * (9.0/5.0) + 32.0
-    return farenheit
+def celciusToFahrenheit(celciusTemperature):
+    fahrenheit = celsiusTemperature * (9.0/5.0) + 32.0
+    return fahrenheit
 
-def farenheitToCelcius (farenheitTemmp):
-    celsius = (farenheitTemperature - 32.0) * (5.0/9.0)
+def fahrenheitToCelcius (fahrenheitTemperature):
+    celsius = (fahrenheitTemperature - 32.0) * (5.0/9.0)
     return celcius
 
+def showMenu():
+    print "A: Convert celcius to fahrenheit"
+    print "B: Convert fahrenheit to celcius"
+    print "X: Exit"
 
+showMenu()
+option = raw_input("option: ")
+while option !="X":
+    if option = "A":
+    value = input("Temperature to convert: ")
+        print (celciusToFahrenheit(float(value)))
